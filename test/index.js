c78b71abcafba6d5b68474094e65fc77a22a5009
Ratson <github@ratson.name>
2018-07-01 16:38:52 +0800

    Test default options

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
 import test from 'ava'
+import http from 'http'
+import getPort from 'get-port'
 
 import connectPort from '..'
 
@@ -8,6 +10,19 @@ test('resolve to socket', async t => {
   })
   t.is(socket.connecting, false)
   t.truthy(socket.on)
+})
+
+test('connect default to localhost', async t => {
+  const port = await getPort()
+  const server = http.createServer((_req, res) => {
+    res.end()
+  })
+  await new Promise(resolve => {
+    server.listen(port, resolve)
+  })
+  const socket = await connectPort(port)
+  t.is(socket.connecting, false)
+  server.close()
 })
 
 test('throws TimeoutError', async t => {
